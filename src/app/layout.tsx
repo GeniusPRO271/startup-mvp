efbdc21da7f0e348311df639018be12d4b47d142
Benjamin Toro <benjamintoroj@gmail.com>
2023-12-17 23:46:13 +0700

    SpeedInsights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import ReduxProvider from '../redux/ReduxProvider'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout(props: {
             {props.children}
             <div id="portal-root"></div>
           </ReduxProvider>
+          <SpeedInsights />
         </body>
     </html>
   )
